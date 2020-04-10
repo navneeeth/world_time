3be492416746126058356190e62e8533058b87c0
navneeeth <44264834+navneeeth@users.noreply.github.com>
2020-04-10 23:19:04 +0530

    Learning Async methods. In choose_location.dart, 1) Created a user-defined void function called getData() with the keyword 'async' before defining it. This allows the scope of asynchronous keywords such as await. In the getData() function, we simulate awaiting network requests for username and the bio of the username. Since both have to be printed in unison, we use the keyword 'await' for both. To simulate the delay, we use the Future.delayed() function and pass the parameters of the duration of the delay and the action to be performed after the delay. Next, we store the responses in a String variable to help in easier output.

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,16 +7,29 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  int counter=0;
+  //int counter=0;
+  void getData() async {
+    //Simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'Yoshi';
+    });
+
+    //Simulate network request to get bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+    return 'Vegan Musician & egg collector';
+    });
+    print('$username--$bio');
+  }
 
   @override
   void initState() {
     super.initState();
+    getData();
     print('initState function ran!');
   }
   @override
   Widget build(BuildContext context) {
-    print('build function ran!');
+    //print('build function ran!');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -24,14 +37,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: Text('Choose a Location'),
         centerTitle: true,
         elevation: 0,
-      ),
-      body: RaisedButton(
-        onPressed: () {
-          setState(() {
-            counter+=1;
-          });
-        },
-        child: Text('Counter is $counter.'),
       ),
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,6 +18,7 @@ class _HomeState extends State<Home> {
               },
               icon: Icon(Icons.edit_location),
               label: Text('Edit Location'),
+              color: Colors.grey,
             ),
           ],
         ),
