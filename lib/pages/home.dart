21cfaf3d6bbee9500b33b5c59d199b4bcbb733c7
navneeeth <44264834+navneeeth@users.noreply.github.com>
2020-04-11 23:45:43 +0530

    Created a list of locations and displayed them with their respective national flags.
    Added a set of flags to the assets folder.
    
    In choose_location.dart,
    1) Added a list of the WorldTime instances of a set of nations with different timezones.
    2) Created a body for the class with a ListView builder with the itemCount as locations.length and the itemBuilder function defined with a symmetric padding and Cards as its children. Created a ListTile for the Cards with an undefined onTap() function and a title consisting of the name of the Location. The leading image is a CircleAvatar that consists of a background image which is a flag asset.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,41 +15,65 @@ class _HomeState extends State<Home> {
     data = ModalRoute.of(context).settings.arguments;
     print(data);
 
+    //set Background
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDaytime'] ? Color(0xff1388C8) : Color(0xff282761);
+
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: <Widget>[
-              FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location'),
-                //color: Colors.grey,
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data['location'],
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[300],
+                  ),
+                  label: Text(
+                    'Edit Location',
                     style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
+                      color: Colors.grey[300],
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 66.0,
+                  //color: Colors.grey,
                 ),
-              ),
-            ],
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
